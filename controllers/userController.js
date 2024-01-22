42385bffdfb165e0cd6453dcd96de3a4a612010b
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-01-22 22:26:29 +0100

    implement route to display users by dateAdded

--- controllers/userController.js
+++ controllers/userController.js
@@ -161,9 +161,40 @@ const getUsersRegex = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+const getUsersByDateAdded = async (req, res) => {
+  try {
+    const { dateAdded } = req.query;
+
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(dateAdded)) {
+      return res
+        .status(400)
+        .json({ error: "Invalid date format for dateAdded" });
+    }
+
+    const startDate = new Date(dateAdded);
+    const endDate = new Date(dateAdded);
+    endDate.setDate(endDate.getDate() + 1);
+
+    const filter = { dateAdded: { $gte: startDate, $lt: endDate } };
+
+    const users = await User.find(filter, "-password");
+
+    if (users.length === 0) {
+      return res.status(404).json({ error: "No user records found" });
+    }
+
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   login,
   register,
   getUsersRegex,
   getUsersExact,
+  getUsersByDateAdded,
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,5 +7,6 @@ router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.get("/users/exact", userController.getUsersExact);
 router.get("/users/regex", userController.getUsersRegex);
+router.get("/users/by-dateAdded", userController.getUsersByDateAdded);
 
 module.exports = router;
