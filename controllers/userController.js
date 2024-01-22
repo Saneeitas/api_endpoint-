03d1cf114fc23021e8581bc2ee7b27bb6c310984
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-01-22 20:52:14 +0100

    implement register route

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,27 @@
 const User = require("../models/User");
 const authService = require("../services/authService");
 
-const register = (req, res) => {
-  res.send("Register")
+const register = async (req, res) => {
+  try {
+    const { firstName, lastName, username, password } = req.body;
+
+    // Check if the username is already taken
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    // Create a new user
+    const newUser = new User({ firstName, lastName, username, password });
+    await newUser.save();
+
+    // Generate a JWT token for the new user
+    const token = authService.generateToken(newUser._id);
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 const login = (req, res) => {
